1569f79b144fd32cb0bb272fbf506ae32350e852
maxime <44418653+mamamaxi@users.noreply.github.com>
2023-09-18 16:14:10 -0400

    the suffering continues
    aaaaaaaaggghhhhhh

--- Array.h
+++ Array.h
@@ -1,70 +1,80 @@
 namespace array {
-    class Array
-    {
+template <typename T, size_t size> class Array {
+    private:
+        T Arr[]; 
     public:
-    //changer les array pour le taleau, ils utilise ce namespace en ce moment
-        Array();
-        int size(array tab);            //renvoie le size du tableau envoyé
-        bool empty(array tab);          //vérifie si le tableau est vide
-        //operator[]                    //prend la valeur à [x]
-        //.at()                         //prend la valeur à (x) avec verification
-        volatile front(array tab);      //valeur du premier element
-        volatile back(array tab);       //valeur du dernier element 
-        void fill(volatile value, array tab);   //met la valeur choisi dans chaque cellules du tableau
-        void swap(array tab1, array tab2);      //échange les valeurs des deux tableaux
-        array fusion(array tab1, array tab2);   //fusionne 2 tableaux
-        array subset(array entry, array retour,int start, int length);   //crée un nouveau tableau avec les valeurs entre start et start + length et le met dans le tableau retour
-        int begin();                    //retourne un iterateur au début du tableau
-        int cbegin();                   //retourne un iterateur constant au début du tableau
-        int end(array tab);             //retourne un iterateur a la fin du tableau
-        int cend(array tab);            //retourne un iterateur constant a la fin du tableau
-    };
-    Array::Array()
-    {
-
-    }
-    Array::size(array tab){
-        int size = 0;
-        //int size = tab.length()
-        return size;
-    }
-    Array::empty(array tab){
-        /*if (tab is empty())
-        {
-            return true
+        Array::Array(){ // non initialisé
+            Arr = nullptr;
+            size = 0
+        };
+        Array::Array(typename Type, int howBigIsThisArray){ // avec initialisation
+            size = howBigIsThisArray;
+            Arr = Type[size]
         }
-        */
-        return false;
-    }
-    Array::front(array tab){
-        return 1;
-    }
-    Array::back(array tab){
-        return 1;
-    }
-    Array::swap(array tab1, array tab2){
 
-    }
-    Array::fusion(array tab1, array tab2){}
-    Array::subset(array entry, array retour, int start, int length){}
-    Array::fill(volatile value, array tab){
-        /*for (int i = 0; i < tab.length; i++)
-        {
-            tab[i] = value
-        }*/
-    }
-    Array::begin(){
-        return 0;
-    }
-    Array::cbegin(){
-        const int i = 0;
-        return i;
-    }
-    Array::end(array tab){
-        return 101;
-    }
-    Array::cend(array tab){
-        return 102;
-    }
-    
+        int size(Array tab){
+            return size;
+        };  //renvoie le size du tableau envoyé
+
+        bool empty(Array tab){
+            if (size == 0)
+            {
+                return true;
+            }
+            return false;
+        }; //vérifie si le tableau est vide
+
+        //operator[], prend la valeur à [x]
+        template <typename T>
+        T* Array<T>::operator[](int x){
+            return Arr[x];
+        }
+
+        //prend la valeur à (x) avec verification
+        T* Array::at(int x){
+            return Arr[x]
+        }
+
+        T* Array::front(Array tab){
+            return tab[0];
+        }      //valeur du premier element
+        T* Array::back(Array tab){
+            return tab[size - 1];
+        }       //valeur du dernier element 
+        void fill(volatile value, Array tab){
+            for (size_t i = 0; i < size; i++)
+            {
+                tab[i] = value
+            }
+            
+        }   //met la valeur choisi dans chaque cellules du tableau
+        void swap(Array tab1, Array tab2){
+            if (tab1.size == tab2.size)
+            {
+                T tab3[size] = tab1;
+                tab1 = tab2;
+                tab2 = tab3;
+                
+            }
+        }     //échange les valeurs des deux tableaux
+
+        Array fusion(Array tab1, Array tab2){
+
+        }  //fusionne 2 tableaux
+        Array subset(Array entry, Array retour,int start, int length){
+
+        }   //crée un nouveau tableau avec les valeurs entre start et start + length et le met dans le tableau retour
+        T* begin(){
+
+        }                   //retourne un iterateur au début du tableau
+        const T* cbegin(){
+
+        }                  //retourne un iterateur constant au début du tableau
+        T* end(Array tab){
+
+        }           //retourne un iterateur a la fin du tableau
+        const T* cend(Array tab){
+            
+        }           //retourne un iterateur constant a la fin du tableau
+    };
 }
