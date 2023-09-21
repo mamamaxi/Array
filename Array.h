4aa504d93ac96779a94a2ee7596e82af48a4fa6f
maxime <44418653+mamamaxi@users.noreply.github.com>
2023-09-21 11:08:32 -0400

    im gonna die

--- Array.h
+++ Array.h
@@ -1,22 +1,46 @@
-namespace array {
-template <typename T, size_t size> class Array {
+#include <stdexcept>
+
+template <typename T, size_t dim> class Array {
     private:
-        T Arr[]; 
+        T* Arr;
+        size_t grandeur;
     public:
         Array::Array(){ // non initialisé
-            Arr = nullptr;
-            size = 0
+            Arr = new T[dim];
+            grandeur = dim;
         };
-        Array::Array(typename Type, int howBigIsThisArray){ // avec initialisation
-            Arr = Type[size]
+
+        Array::~Array(){
+
         }
 
-        int size(Array tab){
-            return size;
+        Array::Array(T tab[]){ // avec initialisation
+            Arr = tab;
+        };
+
+        Array::Array(T* tab[]){ // avec initialisation
+            Arr = tab;
+        };
+
+        Array::Array(const Array& a){
+            Arr = new T[a.grandeur];
+            grandeur = a.grandeur;
+            for (size_t i = 0; i < grandeur; i++)
+                Arr[i] = a[i];
+        };
+
+        Array::Array(Array&& a){
+            Arr = a.Arr;
+            a.Arr = nullptr;
+            grandeur = a.grandeur;
+        };
+
+        size_t size(Array tab){
+            return tab.grandeur;
         };  //renvoie le size du tableau envoyé
 
         bool empty(Array tab){
-            if (size == 0)
+            if (grandeur == 0)
             {
                 return true;
             }
@@ -26,54 +50,75 @@ template <typename T, size_t size> class Array {
         //operator[], prend la valeur à [x]
         template <typename T>
         T* Array<T>::operator[](int x){
-            return Arr[x];
-        }
+            return *Arr[x];
+        };
 
         //prend la valeur à (x) avec verification
         T* Array::at(int x){
-            return Arr[x]
-        }
+            if (x >= size)
+                throw "size exceded";
+            return *Arr[x];
+        };
 
         T* Array::front(Array tab){
             return tab[0];
-        }      //valeur du premier element
+        };//valeur du premier element
+
         T* Array::back(Array tab){
-            return tab[size - 1];
-        }       //valeur du dernier element 
-        void fill(volatile value, Array tab){
+            return tab[grandeur - 1];
+        };//valeur du dernier element 
+
+        void Array::fill(T value, Array tab){
             for (size_t i = 0; i < size; i++)
             {
                 tab[i] = value
             }
-            
-        }   //met la valeur choisi dans chaque cellules du tableau
+        };//met la valeur choisi dans chaque cellules du tableau
+
         void swap(Array tab1, Array tab2){
-            if (tab1.size == tab2.size)
+            if (tab1.grandeur == tab2.grandeur)
             {
-                T tab3[size] = tab1;
+                T *tmp[grandeur] = tab1;
                 tab1 = tab2;
-                tab2 = tab3;
-                
+                tab2 = tmp;
             }
-        }     //échange les valeurs des deux tableaux
+        };//échange les valeurs des deux tableaux
 
         Array fusion(Array tab1, Array tab2){
-
-        }  //fusionne 2 tableaux
-        Array subset(Array entry, Array retour,int start, int length){
-
-        }   //crée un nouveau tableau avec les valeurs entre start et start + length et le met dans le tableau retour
-        T* begin(){
-
-        }                   //retourne un iterateur au début du tableau
-        const T* cbegin(){
-
-        }                  //retourne un iterateur constant au début du tableau
-        T* end(Array tab){
-
-        }           //retourne un iterateur a la fin du tableau
-        const T* cend(Array tab){
+            if (tab1 != tab2)
+                throw "not compatible types"
             
-        }           //retourne un iterateur constant a la fin du tableau
-    };
-}
+            Array tab = new Array<typename T, size_t tab1.grandeur + tab2.grandeur>;
+
+        };//fusionne 2 tableaux
+
+        Array subset(Array entry, int start, int length){
+
+        };//retourne un nouveau tableau avec les valeurs entre start et start + length
+
+        Iterator begin(){
+            return Iterator();
+        };//retourne un iterateur au début du tableau
+
+        const Iterator cbegin(){
+            return Iterator();
+        };//retourne un iterateur constant au début du tableau
+
+        Iterator end(Array tab){
+            return Iterator();
+        };//retourne un iterateur a la fin du tableau
+
+        const Iterator cend(Array tab){
+            return Iterator();
+        };//retourne un iterateur constant a la fin du tableau
+
+        class Iterator{
+        public:
+            Iterator::Iterator(/* args */){
+
+            };
+            Iterator::~Iterator(){
+
+            };
+        };        
+};
